af13fbbb982d5b50b249a7a28fd4127844be9fcd
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-24 07:04:32 -0800

    0x0A: Task1 Puppet Script 1st Fix.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # Installing a flask package from pip3 with version 2.1.0.
 package { 'flask':
     ensure   => '2.1.0',
-    name     => 'flask',
-    provider => pip3
+    provider => 'pip3'
+}
+
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3'
 }
