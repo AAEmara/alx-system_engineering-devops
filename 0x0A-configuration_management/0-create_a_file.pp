787820417c5d8478a2f574ed5c726f679c12be58
AAEmara <abdelrahmanemara18@gmail.com>
2024-01-19 06:11:26 -0800

    0x0A: Task0 Puppet manifest - 1st Fix.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,6 @@
+# Puppet manifest to create a `/tmp/school` file
+# with `0744` permission, File's owner and group is `www-data`;
+# with content that says "I love Puppet".
 file { '/tmp/school':
     path    => '/tmp/school',
     mode    => '0744',
