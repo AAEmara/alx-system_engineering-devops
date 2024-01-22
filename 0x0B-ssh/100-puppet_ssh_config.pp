8a55338f6128ce686feb345d6a086a5f5bff26e6
AAEmara <abdelrahmanemara18@gmail.com>
2024-01-21 18:57:59 -0800

    0x0B: Task4 Puppet manifest.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Adding a Configuration line for `ssh_config` file.
-file_line {'configuring /etc/ssh/ssh_config':
+file {'configuring /etc/ssh/ssh_config':
     path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
+    content => 'IdentityFile ~/.ssh/school',
 }
