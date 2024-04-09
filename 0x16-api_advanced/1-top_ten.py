e830951158ae6e9eaec8527f3c457829b2ec7400
AAEmara <abdelrahmanemara18@gmail.com>
2024-04-09 19:56:40 +0200

    0x16: Task1 Python Script - Top 10 hot posts.
    Added a Python Script to query the Reddit API
    to retrieve the top 10 hot posts for a given
    subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""1-top-ten Module for querying the top 10 posts for a subreddit."""
-
+"""A Module that Queries the Reddit API"""
 
 import requests
-import json
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and Prints the title of the first 10 hot
-    posts listed for a given subreddit."""
-    subred_params = {'exact': True,
-                     'query': subreddit}
-    check_url = 'https://www.reddit.com/api/search_reddit_names.json'
-    subred_check = requests.get(check_url,
-                                params=subred_params)
-    subred_check_text = subred_check.text
-    subred_obj = json.loads(subred_check_text)
-    if (subred_obj['names'][0]):
-        hot_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-        subred_hot = requests.get(hot_url)
-        hot_obj = json.loads(subred_hot.text)
-        hot_list = hot_obj['data']['children']
-        titles_list = list()
-        i = 0
-        for post in hot_list:
-            print(post['data']['title'])
-            i += 1
-            if i == 10:
-                return
-    else:
+    """Queries Reddit's API and prints the titles of the first 10 hot posts."""
+    top_10 = {"limit": 9}
+    website = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(website, params=top_10, allow_redirects=False)
+    if response.status_code == 404:
         print(None)
+        exit()
+    py_objs = response.json()
+    data_objs = py_objs["data"]
+    children_objs = data_objs["children"]
+    for obj in children_objs:
+        print(obj["data"]["title"])
