3cf91b0dc994d102640633ac5cc638046926812c
AAEmara <abdelrahmanemara18@gmail.com>
2024-04-09 17:30:34 +0200

    0x16: Added Reddit API Query - Requests Module
    Used Requests Module to query the Reddit API to
    access the number of subscribers for a given
    subreddit.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""0-subs Module that queries a Reddit API."""
-
+"""A Module to query the Reddit API."""
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and Reutrns the number of subscribers."""
-    subred_params = {'exact': True,
-                     'query': subreddit}
-    check_url = 'https://www.reddit.com/api/search_reddit_names.json'
-    subred_check = requests.get(check_url,
-                                params=subred_params)
-    subred_check_text = subred_check.content.text
-    subred_obj = json.loads(subred_check_text)
-    subred_check_name = subred_check_dict['names'][0]
-    if (subred_check_name):
-        subred_info_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        subred_info = requests.get(subred_info_url)
-        subred_text = subred_info.content.text
-        subred_obj = json.loads(subred_text)
-        subred_subs = subred_obj['data']['subscribers']
-    else:
-        subred_subs = 0
-
-    return (subred_subs)
+    """Querying for the number of subscribers for a specific subreddit."""
+    website = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(website, allow_redirects=False)
+    if response.status_code == 404:
+        return (0)
+    py_objs = response.json()
+    data_objs = py_objs["data"]
+    subscribers_count = data_objs["subscribers"]
+    return (subscribers_count)
